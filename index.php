d95ac799a9fbbae3ffcfeb96c02da6cac5ecaf3e
Jeff McGrath <jeff@youtah.com>
2015-02-07 22:05:40 -0700

    Starting point
    adding index and css file to repo

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Utah HAM Radio Repeaters</title>
+    <meta name="description" content="Utah HAM Radio Repeaters">
+    <meta name="keywords" content="Utah, HAM, HAM RADIO, Operators, Repeaters, amateur radio, amateue, Repeater Systems">
+    <meta name="author" content="Jeff McGrath">
+    <link rel="stylesheet" href="main.css" media="all" type="text/css" />
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+    <script>
+    $(function() {
+        $( "#googlemaps-tab" ).tabs();
+    });
+    </script>
+</head>
+<body>
+    <div id="container" class="rounded-corners">
+        <h1>Utah HAM Radio Repeater Maps</h1>
+        <hr>
+        <article>
+            <p>A collection of Utah amateur HAM radio repeaters and systems displayed on Google Maps.</p>
+        </article>
+        <section id="maps">
+            <div id="googlemaps-tab">
+                <ul>
+                    <li><a href="#googlemaps-tab1">2 Meter</a></li>
+                    <li><a href="#googlemaps-tab2">70CM</a></li>
+                    <li><a href="#googlemaps-tab3">Intertie</a></li>
+                    <li><a href="#googlemaps-tab4">SDARC Sinbad</a></li>
+                    <li><a href="#googlemaps-tab5">1.25CM</a></li>
+                    <li><a href="#googlemaps-tab6">6 Meter</a></li>
+                    <li><a href="#googlemaps-tab7">23CM</a></li>
+                    <li><a href="#googlemaps-tab8">33CM</a></li>
+                </ul>
+                <div id="googlemaps-tab1" class="map-div">
+                    <p><i>More info to come...</i></p>
+                    <iframe
+                    class="map-iframe"
+                    frameborder="0" style="border:0"
+                    src="https://mapsengine.google.com/map/embed?&mid=zaXaLzxN782g.kbY2MagcIIIY">
+                </iframe>
+            </div>
+            <div id="googlemaps-tab2" class="map-div">
+                <p><i>More info to come...</i></p>
+                <iframe
+                class="map-iframe"
+                frameborder="0" style="border:0"
+                src="https://mapsengine.google.com/map/embed?&mid=zaXaLzxN782g.khafwLa9JIfU">
+            </iframe>
+        </div>
+        <div id="googlemaps-tab3" class="map-div">
+            <p><i>More info to come...</i></p>
+            <iframe
+            class="map-iframe"
+            frameborder="0" style="border:0"
+            src="https://mapsengine.google.com/map/embed?&mid=zaXaLzxN782g.kuFTu3PJ38jI">
+        </iframe>
+    </div>
+    <div id="googlemaps-tab4" class="map-div">
+        <p><i>More info to come...</i></p>
+        <iframe
+        class="map-iframe"
+        frameborder="0" style="border:0"
+        src="https://mapsengine.google.com/map/embed?&mid=zaXaLzxN782g.kMD-d26hLMwk">
+    </iframe>
+</div>
+<div id="googlemaps-tab5" class="map-div">
+    <p>Coming soon.</p>
+</div>
+<div id="googlemaps-tab6" class="map-div">
+    <p>Coming soon.</p>
+</div>
+<div id="googlemaps-tab7" class="map-div">
+    <p>Coming soon.</p>
+</div>
+<div id="googlemaps-tab8" class="map-div">
+    <p>Coming soon.</p>
+</div>
+</div>
+<section>
+    <article>
+        <p>This site is privately owned and operated. This site has no affiliation with any groups.</p>
+    </article>
+    <div class="float-clear"></div>
+</div>
+</body>
+</html>
