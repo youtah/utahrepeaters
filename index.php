a398ace9d1b32f09d4ea9c26bcbf7ae14ff420d2
Jake Bair <orso.zed@gmail.com>
2015-05-22 23:09:10 -0600

    converted site to use bootstrap ui for better phone browser support

--- index.php
+++ index.php
@@ -8,46 +8,65 @@
         <meta name="author" content="Jeff McGrath">
         <link rel="stylesheet" href="main.css" media="all" type="text/css" />
         <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-        <script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
         <script src="main.js"></script>
-        <script>
-        $(function() {
-            $( "#googlemaps-tab" ).tabs();
-        });
-        </script>
+        <!-- bootstrap responsive ui -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>    
+        <!-- end bootstrap -->
+        
     </head>
     <body>
         <div id="container" class="rounded-corners">
-            <h1>Utah HAM Radio Repeater Maps</h1>
-            <hr>
-            <article>
-                <p>A collection of Utah amateur HAM radio repeaters and systems displayed on Google Maps.</p>
-            </article>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#">Utah HAM Repeater Maps</a>
+                    </div>
+                    
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="navbar-collapse">
+                        <!-- Nav tabs -->
+                        <ul class="nav navbar-nav" role="tablist">
+                          <li role="presentation" class="active"><a href="#2M" role="tab" data-toggle="tab">2 meter</a></li>
+                          <li role="presentation"><a href="#70CM" role="tab" data-toggle="tab">70CM</a></li>
+                          <li role="presentation"><a href="#INTER" role="tab" data-toggle="tab">Intertie</a></li>
+                          <li role="presentation"><a href="#SDARC" role="tab" data-toggle="tab">SDARC Sinbad</a></li>
+                          <li role="presentation"><a href="#1-25M" role="tab" data-toggle="tab">1.25 Meter</a></li>
+                          <li role="presentation"><a href="#6M" role="tab" data-toggle="tab">6 Meter</a></li>
+                          <li role="presentation"><a href="#23CM" role="tab" data-toggle="tab">23CM</a></li>
+                          <li role="presentation"><a href="#33CM" role="tab" data-toggle="tab">33CM</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
             <section id="maps">
-                <div id="googlemaps-tab">
-                    <ul>
-                        <li><a href="#googlemaps-tab1" id="2M">2 Meter</a></li>
-                        <li><a href="#googlemaps-tab2" id="70CM">70CM</a></li>
-                        <li><a href="#googlemaps-tab3" id="INTER">Intertie</a></li>
-                        <li><a href="#googlemaps-tab4" id="SDARC">SDARC Sinbad</a></li>
-                        <li><a href="#googlemaps-tab5" id="1-25M">1.25 Meter</a></li>
-                        <li><a href="#googlemaps-tab6" id="6M">6 Meter</a></li>
-                        <li><a href="#googlemaps-tab7" id="23CM">23CM</a></li>
-                        <li><a href="#googlemaps-tab8" id="33CM">33CM</a></li>
-                    </ul>
-                    <div id="googlemaps-tab1" class="map-div">&nbsp;</div>
-                    <div id="googlemaps-tab2" class="map-div">&nbsp;</div>
-                    <div id="googlemaps-tab3" class="map-div"></div>
-                    <div id="googlemaps-tab4" class="map-div"></div>
-                    <div id="googlemaps-tab5" class="map-div"></div>
-                    <div id="googlemaps-tab6" class="map-div"></div>
-                    <div id="googlemaps-tab7" class="map-div"></div>
-                    <div id="googlemaps-tab8" class="map-div"></div>
+                <div role="tabpanel">
+                    
+                  
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                      <div role="tabpanel" class="tab-pane active map-div" id="2M"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="70CM"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="INTER"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="SDARC"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="1-25M"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="6M"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="23CM"></div>
+                      <div role="tabpanel" class="tab-pane map-div" id="33CM"></div>
+                    </div>  
                 </div>
             </section>
             <article>
+                <p>A collection of Utah amateur HAM radio repeaters and systems displayed on Google Maps.</p>
                 <p>This site is privately owned and operated. This site has no affiliation with any groups.</p>
             </article>
             <div class="float-clear"></div>
--- mapengine.php
+++ mapengine.php
@@ -1,38 +1,18 @@
 <?php
     $map = $_POST['map'];
     $map_id = '';
-
-    if ($map == '2M') {
-        $map_id = "zaXaLzxN782g.kbY2MagcIIIY";
+    
+    $maps = array(
+        "#2M"    => "zaXaLzxN782g.kbY2MagcIIIY",
+        "#70CM"  => "zaXaLzxN782g.khafwLa9JIfU",
+        "#INTER" => "zaXaLzxN782g.kuFTu3PJ38jI",
+        "#SDARC" => "zaXaLzxN782g.kMD-d26hLMwk"
+    );
+    
+    if (array_key_exists($map, $maps)) {
+        $map_id =$maps[$map];
     }
-
-    if ($map == '70CM') {
-        $map_id = "zaXaLzxN782g.khafwLa9JIfU";
-    }
-
-    if ($map == 'INTER') {
-        $map_id = "zaXaLzxN782g.kuFTu3PJ38jI";
-    }
-
-    if ($map == 'SDARC') {
-        $map_id = "zaXaLzxN782g.kMD-d26hLMwk";
-    }
-
-    if ($map == '1-25M') {
-        $map_id = "";
-    }
-
-    if ($map == '6M') {
-        $map_id = "";
-    }
-
-    if ($map == '33CM') {
-        $map_id = "";
-    }
-
-    if ($map == '23CM') {
-        $map_id = "";
-    }
+    
 
     function make_iframe_map ($map_id){
         $html = "<p><i>More info to come...</i></p>";
